c004ec64f9c08fca4dfbec757774cd8303be2b1a
hacks4snacks <hacks4snacks@protonmail.com>
2019-07-09 13:10:37 -0400

    Moved tge getEventLogSettings script

--- getEventLogSettings.ps1
+++
@@ -1,2 +0,0 @@
-#Dump configuration settings for local event logs into an autoformatted table
-Get-EventLog -list | Format-Table LogDisplayName,MaximumKilobytes,OverflowAction,MinimumRetentionDays -AutoSize
